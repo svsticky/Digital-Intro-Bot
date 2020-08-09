40553305b5fe50405fbcef30946481b2892ce759
Niels <niels.kwadijk@gmail.com>
2020-08-09 11:38:13 +0200

    Fix bad indentation

--- modules/helper_funtions.py
+++ modules/helper_funtions.py
@@ -2,13 +2,13 @@ from botbuilder.core import CardFactory, TurnContext, MessageFactory
 from botbuilder.schema import CardAction, HeroCard, Mention, ConversationParameters
 
 async def create_channel_conversation(turn_context: TurnContext, teams_channel_id: str, message):
-        params = ConversationParameters(
-            is_group=True,
-            channel_data={"channel": {"id": teams_channel_id}},
-            activity=message
-        )
-        connector_client = await turn_context.adapter.create_connector_client(turn_context.activity.service_url)
-        await connector_client.conversations.create_conversation(params)
+    params = ConversationParameters(
+        is_group=True,
+        channel_data={"channel": {"id": teams_channel_id}},
+        activity=message
+    )
+    connector_client = await turn_context.adapter.create_connector_client(turn_context.activity.service_url)
+    await connector_client.conversations.create_conversation(params)
 
 async def create_personal_conversation(turn_context: TurnContext, user, message, app_id):
     conversation_reference = TurnContext.get_conversation_reference(turn_context.activity)
