2574a0c58f8c417d2a164d706e582206d3f52336
Niels <niels.kwadijk@gmail.com>
2020-09-03 14:25:20 +0200

    Fix oopsiewoopsie

--- bots/sticky_ALFAS_bot.py
+++ bots/sticky_ALFAS_bot.py
@@ -90,7 +90,7 @@ class StickyALFASBot(TeamsActivityHandler):
             await self.switch_committee(turn_context)
             return
         
-        if turn_context.activity.startswith("MentorVrijgeven"):
+        if turn_context.activity.text.startswith("MentorVrijgeven"):
             await self.release_mentor_group(turn_context)
             return
         
